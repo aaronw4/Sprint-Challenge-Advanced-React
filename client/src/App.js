95027f54639f8e194642fda695c5fffe3bcf6b45
Aaron Williams <aaronrw4@yahoo.com>
2021-04-17 22:31:28 -0700

    Updated fetch address

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   }
   
   componentDidMount() {
-    fetch(`http://localhost:5000/api/players`)
+    fetch(`https://arw-player-database.herokuapp.com/api/players`)
     .then(res => res.json())
     .then(res => {
         console.log(res);
