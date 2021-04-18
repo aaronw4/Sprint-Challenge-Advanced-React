1a8d42d526587504677e0c3fec2985fc774c0cf2
Aaron Williams <aaronrw4@yahoo.com>
2021-04-17 21:17:53 -0700

    Updates

---
+++ precache-manifest.bec2a0f24ba85c861b5792a3fa9c24a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef084a2cfea325ebe3651e98fc4ac95f",
+    "url": "/Sprint-Challenge-Advanced-React/index.html"
+  },
+  {
+    "revision": "8f9f870e7cf8b1da584d",
+    "url": "/Sprint-Challenge-Advanced-React/static/css/main.ea363844.chunk.css"
+  },
+  {
+    "revision": "e285322da5ca8ec8cdf1",
+    "url": "/Sprint-Challenge-Advanced-React/static/js/2.89fbf8b8.chunk.js"
+  },
+  {
+    "revision": "8f9f870e7cf8b1da584d",
+    "url": "/Sprint-Challenge-Advanced-React/static/js/main.45c0f457.chunk.js"
+  },
+  {
+    "revision": "bc200e860362e22fbefc",
+    "url": "/Sprint-Challenge-Advanced-React/static/js/runtime~main.33885fdd.js"
+  }
+]);
